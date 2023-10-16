dcb118262d66b8d326189fa25855ae8cdc7ac686
naman-parihar2000 <namanp612@gmail.com>
2023-10-16 18:19:09 +0530

    header part 1

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,45 +11,49 @@ const Header = () => {
         <div className="nav__menu">
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav_link">
+              <a href="#home" className="nav__link active-link">
                 <i className="uil uil-estate nav__icon"></i> Home
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#about" className="nav_link">
+              <a href="#about" className="nav__link">
                 <i className="uil uil-user nav__icon"></i> About
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="skills" className="nav_link">
+              <a href="skills" className="nav__link">
                 <i className="uil uil-file-alt nav__icon"></i> Skills
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#services" className="nav_link">
+              <a href="#services" className="nav__link">
                 <i className="uil uil-briefcase-alt nav__icon"></i>
                 Services
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#portfolio" className="nav_link">
+              <a href="#portfolio" className="nav__link">
                 <i className="uil uil-scenery nav__icon"></i>Portfolio
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#contact" className="nav_link">
+              <a href="#contact" className="nav__link">
                 <i className="uil uil-message nav__icon"></i>
                 Contact
               </a>
             </li>
           </ul>
 
-          <i className="uil uil-times nav-close"></i>
+          <i className="uil uil-times nav__close"></i>
+        </div>
+
+        <div className="nav__toggle">
+          <i className="uil uil-apps"></i>
         </div>
       </nav>
     </header>

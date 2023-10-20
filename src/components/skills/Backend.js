ff92bfb13967202718a841c8645e7457cad0bb2c
naman-parihar2000 <namanp612@gmail.com>
2023-10-20 12:30:04 +0530

    Skills section added

---
+++ src/components/skills/Backend.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Developer</h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <div>
+              <h3 className="skills__name">MongoDB</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <div>
+              <h3 className="skills__name">MySQL</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <div>
+              <h3 className="skills__name">Node.js</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <div>
+              <h3 className="skills__name">Git</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;

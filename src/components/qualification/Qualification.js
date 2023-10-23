a92100946f01119c268c7c4b416be5d01806a312
naman-parihar2000 <namanp612@gmail.com>
2023-10-23 12:52:31 +0530

    Services Section completed

---
+++ src/components/qualification/Qualification.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import "./Qualification.css";
+
+const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <section className="qualification section">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My Journey</span>
+
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button button--flex qualification__active"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            Education
+          </div>
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button button--flex qualification__active"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Happy Hours School</h3>
+                <span className="qualification__subtitle">
+                  Secondary Schooling / 10th
+                </span>
+                <div className="qualification__calendar">2017</div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">GS Jandid</h3>
+                <span className="qualification__subtitle">
+                  Senior Secondary Schooling / 12th
+                </span>
+                <div className="qualification__calendar">2017-2019</div>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Graphic Era Hill University
+                </h3>
+                <span className="qualification__subtitle">
+                  B.Tech Computer Science And Engineering
+                </span>
+                <div className="qualification__calendar">2019-2023</div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+          </div>
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">
+                  AWS Cloud Practitioner Certification
+                </h3>
+                <span className="qualification__subtitle">Amazon</span>
+                <div className="qualification__calendar">2021</div>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Udemy React Cources</h3>
+                <span className="qualification__subtitle">Udemy</span>
+                <div className="qualification__calendar">2022</div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Software Developer</h3>
+                <span className="qualification__subtitle">
+                  Sportix India Private Limited
+                </span>
+                <div className="qualification__calendar">2023-Present</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
